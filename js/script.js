c3f4c7caeb56516ddc35c64f3e22d2a08d8ec0aa
Aidilsur <aidilsurya9@gmail.com>
2022-02-04 18:29:24 +0700

    add bussiness section and footer

--- js/script.js
+++ js/script.js
@@ -5,3 +5,19 @@ const navMobileElement = document.querySelector(".nav-mobile");
 
 menuToggleOpen.addEventListener("click", () => navMobileElement.classList.add("active"));
 menuToggleClose.addEventListener("click", () => navMobileElement.classList.remove("active"));
+
+// accordion
+var acc = document.getElementsByClassName("accordion");
+var i;
+var len = acc.length;
+for (i = 0; i < len; i++) {
+  acc[i].addEventListener("click", function () {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    }
+  });
+}
